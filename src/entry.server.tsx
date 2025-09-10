3150299d03875332dcda3b29d0907da8a68e03be
Meraj7718 <ag1390578@gmail.com>
2025-09-10 17:19:21 +0530

    Render3

--- src/entry.server.tsx
+++ src/entry.server.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
+import { Routes, Route } from "react-router-dom";
 import routes from "./app/routes";
 
 export function render(url: string) {
   return renderToString(
     <StaticRouter location={url}>
-      {routes}
+      <Routes>
+        {routes.map((route: any, idx: number) => (
+          <Route
+            key={idx}
+            path={route.path}
+            element={
+              route.element
+                ? route.element
+                : route.component
+                ? React.createElement(route.component)
+                : null
+            }
+          />
+        ))}
+      </Routes>
     </StaticRouter>
   );
 }
